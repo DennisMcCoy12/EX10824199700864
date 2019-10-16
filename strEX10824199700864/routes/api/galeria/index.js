df3aafa17419984803418293ab8cf0ac800111f8
Dennis <McCoy>
2019-10-15 19:29:02 -0600

    put terminado

--- strEX10824199700864/routes/api/galeria/index.js
+++ strEX10824199700864/routes/api/galeria/index.js
@@ -68,4 +68,36 @@ router.delete(
     }
 );
 
+router.put('/update/:galid',
+  function(req, res){
+      galCollection = fileModel.getGaleria();
+      var galidToModify = req.params.galid;
+
+      var urlToModi = req.body.urlTM;
+      var thumbnailUrlToModi = req.body.thumbnailUrlTM;
+      var modGaleria = {};
+      var newGaleriaArray = galCollection.map(
+        function(o,i){
+          if( galidToModify === o.id){
+             o.url = urlToModi;
+             o.thumbnailUrl=thumbnailUrlToModi;
+             modGaleria = Object.assign({}, o);
+          }
+          return o;
+        }
+      );
+    galCollection = newGaleriaArray;
+    fileModel.setGaleria(
+      galCollection,
+      function (err, savedSuccesfully) {
+        if (err) {
+          res.status(400).json({ "error":"No se pudo modificar la imagen" });
+        } else {
+          res.json(modGaleria);
+        }
+      }
+    );
+  }
+);
+
 module.exports=router;
